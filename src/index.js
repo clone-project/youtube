003b42c32b49c509d8b336d55a7beed74bb1b10e
songee-kim <songee-kim@nts-corp.com>
2021-01-14 18:29:53 +0900

    HELP ME KKOKKO

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './styles/youtube.scss'
+
+var requestUser = '../mock/user.json';
+//var requestRecommendVideo = '../mock/recommendVideo.json';
+//var requestPopularVideo = '../mock/popularVideo.json';
+
+var request = new XMLHttpRequest();
+request.open('GET', requestUser);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  var userInfo = request.response;
+  //var users = JSON.parse(userInfo);
+  getUserInformation(users);
+};
+
+function getUserInformation(jsonObj) {
+  document.getElementById('user-nickname').textContent = jsonObj.user.nickname;
+  console.log(jsonObj.user.nickname);
+}
